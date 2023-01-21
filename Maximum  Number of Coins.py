c272cbdb02a1c57e2e65dc256e4aa5fb1c2011ed
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2023-01-21 10:32:05 -0800

    Create Maximum  Number of Coins.py

---
+++ Maximum  Number of Coins.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxCoins(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: int
+        """
+        piles.sort()
+        return sum(itertools.islice(piles, len(piles)//3, len(piles), 2))
